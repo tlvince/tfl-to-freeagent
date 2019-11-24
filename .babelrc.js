690eb752052ec368cf21b14166b4819b3131678f
Tom Vincent <git@tlvince.com>
2019-11-24 14:46:58 +0000

    chore(github): add fix lint workflow

--- .babelrc.js
+++ .babelrc.js
@@ -3,8 +3,5 @@ module.exports = {
     '@babel/plugin-transform-runtime',
     '@babel/plugin-transform-regenerator',
   ],
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-typescript',
-  ],
-};
+  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+}
